a7c6595e3ee7cb56f741e7178940f2f6c6bc47e8
Sara Tahir <saratahir34@gmail.com>
2025-07-28 13:00:54 +0100

    Assigned state to a variable.

--- prep/script.js
+++ prep/script.js
@@ -1,5 +1,20 @@
+/*Given a view of film cards and search box
+When a user types in the search box
+Then the view should update to show only matching films.
+*/
 
-const films = [
+
+//we need access to user input
+//Event listener for user input
+
+//State- Data that can change over time. 
+//---Our Films Array---
+//__ Our Search term/bar---
+
+ 
+const state = {
+  films:[
+
     {
   title: "Killing of Flower Moon",
   director: "Martin Scorsese",
@@ -13,11 +28,33 @@ const films = [
     times: ["15:00", "20:00"],
     certificate: "12A",
     duration: 108,
-  }];
+  },
+
+    {
+    title: "Friends",
+    director: "Shauna",
+    times: ["15:00", "20:00"],
+    certificate: "12A",
+    duration: 108,
+  },
+    {
+    title: "The Rookie",
+    director: "Madisson",
+    times: ["15:00", "20:00"],
+    certificate: "12A",
+    duration: 108,
+  }
+],
+  searchTerm:"",
+};
+
+
+//State- Data that can change over time. 
+//---Our Films Array---
+//__ Our Search term/bar---
 
 
 
-//How we go through each object in the array and create a card for each object/film.
 
 function createFilmCard(film){
     const filmCard = document
@@ -27,14 +64,10 @@ filmCard.querySelector("h3").textContent = film.title;
 filmCard.querySelector("p").textContent = film.director;
 
 
-//not interested in appending the filmCard to DOM here
+
 return filmCard
 }
 
 const filmCards = films.map(createFilmCard);
 document.body.append(...filmCards);
 
-//document.body.append(createFilmCard(),createFilmCard())
-
-//document.body.append(createFilmCard(films),createFilmCard(film))
- 
